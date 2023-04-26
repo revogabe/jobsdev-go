5a321aac0c67844727a594a34419bb20f40a2335
ArielBetti <ariel.betti@gmail.com>
2023-04-26 17:36:03 -0300

    feat: create a loading page for work router

--- src/app/work/[id]/page.tsx
+++ src/app/work/[id]/page.tsx
@@ -1,10 +1,10 @@
-import { TJobsResponseUnique } from '@/types'
-import { BackpackIcon } from '@radix-ui/react-icons'
+import { TJobsResponseUnique } from '@/types';
+import { BackpackIcon } from '@radix-ui/react-icons';
 
 interface PageProps {
   params: {
-    id: string
-  }
+    id: string;
+  };
 }
 
 export default async function WorkPage({ params }: PageProps) {
@@ -15,9 +15,10 @@ export default async function WorkPage({ params }: PageProps) {
       next: {
         revalidate: 60,
       },
-    },
-  )
-  const { data } = (await response.json()) as TJobsResponseUnique
+    }
+  );
+
+  const { data } = (await response.json()) as TJobsResponseUnique;
   const {
     Title,
     Description,
@@ -29,53 +30,50 @@ export default async function WorkPage({ params }: PageProps) {
     Link,
     Location,
     Approved,
-  } = data
+  } = data;
 
-  console.log(data)
   return Approved ? (
-    <main className="min-h-screen w-full mx-auto px-4 pt-12">
+    <main className="mx-auto min-h-screen w-full px-4 py-12">
       <div className="flex flex-col gap-8">
-        <div className="flex items-start lg:items-center justify-between gap-4 lg:flex-row flex-col">
-          <h1 className="md:text-5xl text-3xl font-bold">{Title}</h1>
+        <div className="flex flex-col items-start justify-between gap-4 lg:flex-row lg:items-center">
+          <h1 className="text-3xl font-bold md:text-5xl">{Title}</h1>
           <div className="flex items-center justify-end gap-3">
-            <p className="flex gap-2 items-center justify-center bg-zinc-900 px-6 py-3 rounded-lg text-lg font-bold">
+            <p className="flex items-center justify-center gap-2 rounded-lg bg-zinc-900 px-6 py-3 text-lg font-bold">
               <BackpackIcon width={24} height={24} /> {Role}
             </p>
             <a
               href={Link}
-              className="flex gap-2 items-center justify-center bg-emerald-500 px-6 py-3 rounded-lg text-lg font-bold text-white hover:bg-emerald-800 hover:text-white duration-150 ease-out active:scale-95"
+              className="flex items-center justify-center gap-2 rounded-lg bg-emerald-500 px-6 py-3 text-lg font-bold text-white duration-150 ease-out hover:bg-emerald-800 hover:text-white active:scale-95"
             >
               Aplicar para Vaga
             </a>
           </div>
         </div>
-        <div className="flex gap-4 items-center justify-start">
-          <p className="cursor-default bg-zinc-800 flex items-center justify-center gap-2 hover:bg-zinc-900 ease-out duration-150 hover:text-emerald-400 px-3 py-1 text-zinc-300 text-base rounded-md">
+        <div className="flex items-center justify-start gap-4">
+          <p className="flex cursor-default items-center justify-center gap-2 rounded-md bg-zinc-800 px-3 py-1 text-base text-zinc-300 duration-150 ease-out hover:bg-zinc-900 hover:text-emerald-400">
             Empresa: {Company}
           </p>
-          <p className="cursor-default bg-zinc-800 flex items-center justify-center gap-2 hover:bg-zinc-900 ease-out duration-150 hover:text-emerald-400 px-3 py-1 text-zinc-300 text-base rounded-md">
+          <p className="flex cursor-default items-center justify-center gap-2 rounded-md bg-zinc-800 px-3 py-1 text-base text-zinc-300 duration-150 ease-out hover:bg-zinc-900 hover:text-emerald-400">
             Localidade: {Location}
+          </p>
+          <p className="flex cursor-default items-center justify-center gap-2 rounded-md bg-zinc-800 px-3 py-1 text-base text-zinc-300 duration-150 ease-out hover:bg-zinc-900 hover:text-emerald-400">
+            {Remote ? "Remote" : "Presencial"}
+          </p>
+          <p className="flex cursor-default items-center justify-center gap-2 rounded-md bg-zinc-800 px-3 py-1 text-base text-zinc-300 duration-150 ease-out hover:bg-zinc-900 hover:text-emerald-400">
+            Experiência: {Experience}
+          </p>
+          <p className="flex cursor-default items-center justify-center gap-2 rounded-md bg-zinc-800 px-3 py-1 text-base text-zinc-300 duration-150 ease-out hover:bg-zinc-900 hover:text-emerald-400">
+            Salário: {Salary}
           </p>
         </div>
       </div>
-      <div className="text-base w-full max-w-3xl mt-8">
+      <div className="mt-8 w-full max-w-3xl text-base">
         <p className="whitespace-pre-line">{Description}</p>
-      </div>
-      <div className="mt-8 flex md:flex-row flex-col gap-4 items-start md:items-center justify-start">
-        <p className="cursor-default bg-zinc-800 flex items-center justify-center gap-2 hover:bg-zinc-900 ease-out duration-150 hover:text-emerald-400 px-3 py-1 text-zinc-300 text-base rounded-md">
-          {Remote ? 'Remote' : 'Presencial'}
-        </p>
-        <p className="cursor-default bg-zinc-800 flex items-center justify-center gap-2 hover:bg-zinc-900 ease-out duration-150 hover:text-emerald-400 px-3 py-1 text-zinc-300 text-base rounded-md">
-          Experiência: {Experience}
-        </p>
-        <p className="cursor-default bg-zinc-800 flex items-center justify-center gap-2 hover:bg-zinc-900 ease-out duration-150 hover:text-emerald-400 px-3 py-1 text-zinc-300 text-base rounded-md">
-          Salário: {Salary}
-        </p>
       </div>
     </main>
   ) : (
-    <main className="min-h-screen w-full flex items-start pt-40 justify-center">
-      <h1 className="bg-zinc-900 px-8 py-4 rounded-lg">Vaga não encontrada</h1>
+    <main className="flex min-h-screen w-full items-start justify-center pt-40">
+      <h1 className="rounded-lg bg-zinc-900 px-8 py-4">Vaga não encontrada</h1>
     </main>
-  )
+  );
 }
